570e152d9870ba37f5a3c18d9d872af3540de223
Melissa Galush <galushmn@gmail.com>
2020-04-12 18:27:45 -0700

    theres a bug

--- todoList.js
+++ todoList.js
@@ -2,23 +2,21 @@ export default class TodoList {
   newTodoText = document.querySelector('.new-todo');
   list = document.querySelector('.todo-list');
   addNewButton = document.querySelector('.add-new');
-  todoList = [];
+  todoList;
 
   constructor() {
     // creates new todo
-    this.addNewButton.addEventListener('click', this.createTodo);
+    this.addNewButton.addEventListener('click', this.addNewButtonClick);
+    this.todoList = this.loadTodos();
+    this.todoList.forEach( (task, index) => {
+      this.renderTodo(task, index);
+    });
   }
 
-  createTodo = () => {
+  renderTodo = (task, index) => {
     //create new todo
     let newTodo = document.createElement('li');
-    newTodo.innerText = this.newTodoText.value;
-
-    // add new todo to todoList
-    this.todoList.push(this.newTodoText.value);
-
-    // determine whre in array the todo index is stored
-    let index = this.todoList.length - 1;
+    newTodo.innerText = task;
 
     // create delete button for new todo
     let deleteButton = document.createElement('button');
@@ -35,12 +33,39 @@ export default class TodoList {
     this.list.appendChild(newTodo);
   };
 
+  addNewButtonClick = () => {
+    // add new todo to todoList
+    this.todoList.push(this.newTodoText.value);
+
+    // determine where in array the todo index is stored
+    let index = this.todoList.length - 1;
+
+    this.renderTodo(this.newTodoText.value, index);
+
+    this.saveTodos();
+  };
+
   // NOTE TO SELF: todoElement is arbitrary argument
   deleteTodo = (todoElement, index) => {
+    console.log(index);
     // delete from array
     this.todoList.splice(index, 1);
-    console.log(this.todoList);
     // delete from DOM
     todoElement.remove();
+    this.saveTodos();
+  };
+
+  // load todos already in the array and return any arrays that exist in local storage
+  loadTodos = () => {
+    const todoList = JSON.parse(localStorage.getItem('todoList'));
+    if (!todoList) {
+      return [];
+    }
+    return todoList;
+  };
+
+  // save entire todo list
+  saveTodos = () => {
+    localStorage.setItem('todoList', JSON.stringify(this.todoList));
   };
 }
